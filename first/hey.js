2244b8e567481e5d9d7d5be81d3c9e8d84412fb5
Devanshu Pareek <154327194+Centrix3009@users.noreply.github.com>
2024-05-25 18:32:25 +0000

    letscommit

--- first/hey.js
+++ first/hey.js
@@ -4,3 +4,8 @@ const arr = [1, 2, 3,]
 const hey = [3,5]
 const lee = [...arr, ...hey]
 console.log(lee)
+
+const score1 = 78
+const score3 = 87
+console.log(Array.of(score1, score3))
+console.log(Array.from("ramset"))
